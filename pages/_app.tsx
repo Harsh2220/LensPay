56aedec2e10a8fbf2ea43ac41a28e96958b77981
Harsh <harshgajjar145@gmail.com>
2023-02-02 20:38:45 +0530

    env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { AuthProvider } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+import type { AppProps } from "next/app";
+
+const provider = new AuthProvider(process.env.NEXT_PUBLIC_ARCANA_KEY);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ProvideAuth provider={provider}>
+      <Component {...pageProps} />
+    </ProvideAuth>
+  );
 }
